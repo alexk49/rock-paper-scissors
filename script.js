30f227fca333923ad38ce89042bd140cf3efc11b
alexk49 <alexcode9@mailbox.org>
2024-05-13 08:58:41 +0100

    added getComputerChoice function which only selects from valid RPS options

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("hello");
-});
+document.addEventListener('DOMContentLoaded', function () {
+  // load all code after the DOM
+  function getComputerChoice () {
+  // get random number between 0 and 3
+  // and index the VALID_CHOICES array
+    const choice = Math.floor(Math.random() * 3)
+    return VALID_CHOICES[choice]
+  }
+
+  const VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+  const computerChoice = getComputerChoice()
+
+  console.log(computerChoice)
+})
