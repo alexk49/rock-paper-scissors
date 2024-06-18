6e94a841f61fe900ddf030c9c944fbdaed49442f
alexk49 <alexcode9@mailbox.org>
2024-06-18 08:25:04 +0100

    basic game works with buttons

--- script.js
+++ script.js
@@ -1,89 +1,99 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // load all code after the DOM
-  function getComputerChoice () {
-    // get random number between 0 and 3
-    // and index the VALID_CHOICES array
-    const choice = Math.floor(Math.random() * 3)
-    return VALID_CHOICES[choice]
+function getComputerChoice () {
+// get random number between 0 and 3
+// and index the VALID_CHOICES array
+  const choice = Math.floor(Math.random() * 3)
+  return VALID_CHOICES[choice]
+}
+
+function getHumanChoice () {
+// get play choice of valid choices
+  let choice = ''
+  while (VALID_CHOICES.includes(choice) === false) {
+    choice = prompt('rock, paper, scissors?').toLowerCase()
   }
+  return choice
+}
 
-  function getHumanChoice () {
-    // get play choice of valid choices
-    let choice = ''
-    while (VALID_CHOICES.includes(choice) === false) {
-      choice = prompt('rock, paper, scissors?').toLowerCase()
-    }
-    return choice
+function computerWins (playerChoice, computerChoice, computerScore) {
+// log computer winner
+  console.log('You lose!', computerChoice, 'beats:', playerChoice)
+  return (computerScore += 1)
+}
+
+function playerWins (playerChoice, computerChoice, humanScore) {
+// log player winner
+  console.log('You win!', playerChoice, 'beats:', computerChoice)
+  return (humanScore += 1)
+}
+
+function draw (playerChoice, computerChoice) {
+// log draw
+  console.log("It's a draw - player went:", playerChoice, 'and computer went', computerChoice)
+}
+
+function playRound (playerChoice, computerChoice, playerScore, computerScore) {
+// rock beats paper but loses to paper
+// paper beats rock but loses to scissors
+// scissors beats paper but loses to rock
+  if (playerChoice === computerChoice) {
+    draw(playerChoice, computerChoice)
+  } else if (playerChoice === 'rock' && computerChoice !== 'paper') {
+    playerScore = playerWins(playerChoice, computerChoice, playerScore)
+  } else if (playerChoice === 'paper' && computerChoice !== 'scissors') {
+    playerScore = playerWins(playerChoice, computerChoice, playerScore)
+  } else if (playerChoice === 'scissors' && computerChoice !== 'rock') {
+    playerScore = playerWins(playerChoice, computerChoice, playerScore)
+  } else {
+    computerScore = computerWins(playerChoice, computerChoice, computerScore)
   }
+  return [playerScore, computerScore]
+}
 
-  function computerWins (playerChoice, computerChoice, computerScore) {
-    // log computer winner
-    console.log('You lose!', computerChoice, 'beats:', playerChoice)
-    return (computerScore += 1)
-  }
+function playGame () {
+// play whole game of 5 rounds
+// let round = 1
 
-  function playerWins (playerChoice, computerChoice, humanScore) {
-    // log player winner
-    console.log('You win!', playerChoice, 'beats:', computerChoice)
-    return (humanScore += 1)
-  }
+  let playerScore = 0
+  let computerScore = 0
 
-  function draw (playerChoice, computerChoice) {
-    // log draw
-    console.log("It's a draw - player went:", playerChoice, 'and computer went', computerChoice)
-  }
+  // while (round < 5) {
+  console.log("let's play!")
 
-  function playRound (playerChoice, computerChoice, playerScore, computerScore) {
-    // rock beats paper but loses to paper
-    // paper beats rock but loses to scissors
-    // scissors beats paper but loses to rock
-    if (playerChoice === computerChoice) {
-      draw(playerChoice, computerChoice)
-    } else if (playerChoice === 'rock' && computerChoice !== 'paper') {
-      playerScore = playerWins(playerChoice, computerChoice, playerScore)
-    } else if (playerChoice === 'paper' && computerChoice !== 'scissors') {
-      playerScore = playerWins(playerChoice, computerChoice, playerScore)
-    } else if (playerChoice === 'scissors' && computerChoice !== 'rock') {
-      playerScore = playerWins(playerChoice, computerChoice, playerScore)
-    } else {
-      computerScore = computerWins(playerChoice, computerChoice, computerScore)
-    }
-    return [playerScore, computerScore]
-  }
+  const computerChoice = getComputerChoice()
 
-  function playGame () {
-    // play whole game of 5 rounds
-    let round = 1
+  const playerChoice = getHumanChoice()
 
-    let playerScore = 0
-    let computerScore = 0
+  console.log('Computer choice:', computerChoice)
+  console.log('player choice:', playerChoice)
 
-    while (round < 5) {
-      console.log("let's play!")
+  const scores = playRound(playerChoice, computerChoice, playerScore, computerScore)
 
-      const computerChoice = getComputerChoice()
+  playerScore = scores[0]
+  computerScore = scores[1]
 
-      const playerChoice = getHumanChoice()
+  round += 1
 
-      console.log('Computer choice:', computerChoice)
-      console.log('player choice:', playerChoice)
+  console.log("it's round:", round)
+  console.log('Your score is:', playerScore)
+  console.log('Computer score is:', computerScore)
 
-      const scores = playRound(playerChoice, computerChoice, playerScore, computerScore)
+  console.log('final scores. Computer:', computerScore, 'player:', playerScore)
+}
 
-      playerScore = scores[0]
-      computerScore = scores[1]
+const VALID_CHOICES = ['rock', 'paper', 'scissors']
 
-      round += 1
+// playGame()
 
-      console.log("it's round:", round)
-      console.log('Your score is:', playerScore)
-      console.log('Computer score is:', computerScore)
-    }
+const choiceButtons = document.querySelectorAll('.choice')
 
-    console.log('final scores. Computer:', computerScore, 'player:', playerScore)
-  }
+const playerScore = 0
+const computerScore = 0
 
-  const VALID_CHOICES = ['rock', 'paper', 'scissors']
-
-  playGame()
+choiceButtons.forEach((button) => {
+  // create click listener for each button
+  button.addEventListener('click', () => {
+    playerChoice = button.id
+    const computerChoice = getComputerChoice()
+    playRound(playerChoice, computerChoice, playerScore, computerScore)
+  })
 })
